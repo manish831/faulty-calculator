3ac2aa803a6c6b3c8472de408af9da65cafadfb7
manish831 <manish.agrahari2015@gmail.com>
2020-11-25 16:57:17 +0530

    Add files via upload
    maza aa jayga

---
+++ faulty calculator.py
@@ -0,0 +1,23 @@
+var1 = int(input("enter first no.: "))
+var2 = int(input("enter second no.: "))
+operator = input("choose operator: ")
+if operator == '+':
+    if  var1==56 and var2==9:
+        print("77")
+    else:
+        print("sum is",var1+var2)  
+elif operator == '-':
+    if var1==43 and var2==5:
+        print("36")
+    else:
+        print("difference is",var1-var2)
+elif operator == '*':
+    if var1==45 and var2==3:
+        print("555")
+    else:
+        print("multiplication is",var1*var2)
+elif operator == '/':
+    if var1==56 and var2==6:
+        print("4")
+    else:
+        print("division is ",var1/var2)           
